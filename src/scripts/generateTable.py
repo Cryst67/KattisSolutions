25061d90b112816acb555cfb60687d06ea4a3a75
Aryan S <aryan@beaches.app>
2023-06-02 18:19:50 +0530

    feat: add difficulty, sort alphabetically

--- src/scripts/generateTable.py
+++ src/scripts/generateTable.py
@@ -10,6 +10,7 @@ ext_to_logo = {
 
 base_dir = 'src/problems/'
 problem_dirs = [f for f in os.listdir(base_dir) if os.path.isdir(os.path.join(base_dir, f))]
+problem_dirs.sort()
 
 table_rows = []
 
@@ -18,6 +19,7 @@ for problem in problem_dirs:
         lines = f.readlines()
         name = [line.split(": ")[1].strip() for line in lines if "Name" in line][0]
         url = [line.split(": ")[1].strip() for line in lines if "URL" in line][0]
+        difficulty = [line.split(": ")[1].strip() for line in lines if "Difficulty" in line][0]
         problem_link = f"[{name}](https://open.kattis.com{url})"
     solution_links = []
     solution_files = os.listdir(os.path.join(base_dir, problem, 'solutions'))
@@ -27,10 +29,10 @@ for problem in problem_dirs:
         solution_path = f"{base_dir}/{problem}/solutions/{solution}"
         solution_link = f"<a href='{solution_path}'><img src='assets/logos/{logo}' width='30'></a>"
         solution_links.append(solution_link)
-    table_rows.append(f"| {problem_link} | {' '.join(solution_links)} |")
+    table_rows.append((name, f"| {problem_link} | {' '.join(solution_links)} | {difficulty} |"))
 
 with open('TABLE.md', 'w') as f:
-    f.write("\n| Problem | Solutions |\n")
-    f.write("| --- | --- |\n")
-    for row in table_rows:
+    f.write("\n| Problem | Solutions | Difficulty |\n")
+    f.write("| --- | --- | --- |\n")
+    for _, row in table_rows:
         f.write(row + "\n")
